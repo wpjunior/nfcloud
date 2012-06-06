43d58adc8020c568eabdb0e91b5d582b2bf2ddbe
Wilson Júnior <wilsonpjunior@gmail.com>
2012-06-06 12:22:40 -0300

    path for static

--- nfcloud/settings.py
+++ nfcloud/settings.py
@@ -1,5 +1,8 @@
 # Django settings for nfcloud project.
 
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -59,7 +62,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
