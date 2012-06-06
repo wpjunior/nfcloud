fd395a43343ffdfa47f9d9479a820302a1edf250
Wilson Júnior <wilsonpjunior@gmail.com>
2012-06-06 12:15:28 -0300

    fixes

--- manage.py
+++ manage.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python2
 import os
 import sys
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, os.path.join(BASE_DIR, 'packages'))
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nfcloud.settings")
